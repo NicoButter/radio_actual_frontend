7037cd5442a6385acf9290aac1d5676b98e72ccb
NicoButter <nicobutter@gmail.com>
2024-01-09 20:32:44 -0300

    proxycors solucionado y se actualiza cada 5 minutos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 // Definir la función que carga las noticias
 function cargarNoticias() {
-    const rssFeedURL = 'https://www.santacruzalmomento.com.ar/feed/';
-    /*const rssFeedURL = 'https://corsproxy.io?https://www.santacruzalmomento.com.ar/feed/';*/
+    const rssFeedURL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A//www.santacruzalmomento.com.ar/feed/';
+
+    fetch(rssFeedURL)
+        .then(response => response.json())  // Parsear la respuesta como JSON
+        .then(data => {
+            const items = data.items;
+            const newsList = document.getElementById('news-list');
+
+            // Limpiar la lista antes de agregar nuevas noticias
+            newsList.innerHTML = '';
+
+            items.forEach(item => {
+                const title = item.title;
+                const link = item.link;
+
+                const listItem = document.createElement('li');
+                listItem.classList.add('news-item');
+                listItem.innerHTML = `
+                    <i class="fas fa-newspaper"></i>
+                    <a href="${link}" target="_blank">${title}</a>
+                `;
+                newsList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener el feed RSS:', error);
+        });
+}
+
+// Ejecutar la función cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function () {
+    // Cargar noticias inicialmente
+    cargarNoticias();
+
+    // Recargar noticias cada 5 minutos
+    setInterval(cargarNoticias, 300000);
+});
+
+
+
+
+
+
+
+
+
+/*// Definir la función que carga las noticias
+function cargarNoticias() {
+    /*const rssFeedURL = 'https://www.santacruzalmomento.com.ar/feed/';
+    const rssFeedURL = 'https://corsproxy.io/?https://www.santacruzalmomento.com.ar/feed/';
 
     fetch(rssFeedURL)
         .then(response => response.text())
@@ -39,4 +87,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Recargar noticias cada 5 segundos
     setInterval(cargarNoticias, 5000);
-});
+});*/
